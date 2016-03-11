b3c48b9e70c08c146cc02c6cba0a48b83e862ab5
Subham <sbhm94@gmail.com>
2016-03-11 16:33:35 +0530

    first commit

---
+++ ls-a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <dirent.h>
+#include <sys/stat.h>
+using namespace std;
+int main()
+{
+	DIR *dir;
+	struct dirent *file;
+	dir=opendir("/home/bootstrap");
+	if(dir != NULL)
+	{
+		while(file=readdir(dir))
+		{
+			
+			struct stat st;
+			//stat(file->d_name,&st);
+			cout<<file->d_name<<endl;
+		}
+		closedir(dir);
+	}
+	else
+	{
+		cout<<"Could not open directory.";
+	}
+	return 0;
+}
